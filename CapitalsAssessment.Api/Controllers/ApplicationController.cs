b42f36a27a83d8c7638065818db26a8e1e54e53c
balogunpraise <balogunpraise2@gmail.com>
2024-05-14 11:29:41 +0100

    added controller

---
+++ CapitalsAssessment.Api/Controllers/ApplicationController.cs
@@ -0,0 +1,40 @@
+﻿using CapitalsAssessment.Core.APIResponse;
+using CapitalsAssessment.Core.Dtos;
+using CapitalsAssessment.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapitalsAssessment.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationController(IEmployeeApplicationRepository employeeApplicationRepository) : ControllerBase
+    {
+        private readonly IEmployeeApplicationRepository _employeeApplicationRepository = employeeApplicationRepository;
+
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResponse<EmployeeApplicationDto>>> Apply(EmployeeApplicationDto input)
+        {
+            var result = await _employeeApplicationRepository.ApplyAsync(input.ProgramId, input);
+            if (result is not null)
+                return BaseResponse<EmployeeApplicationDto>.Success("Success", result, 200);
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<BaseResponse<EmployeeApplicationDto>>> UpdateApplication(EmployeeApplicationDto input)
+        {
+            var result = await _employeeApplicationRepository.UpdateApplication(input);
+            if (result is not null)
+                return BaseResponse<EmployeeApplicationDto>.Success("Success", result, 200);
+            return BadRequest();
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult GetEmployeeApplicationById(string id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ CapitalsAssessment.Api/Controllers/ProgramController.cs
@@ -0,0 +1,37 @@
+﻿using CapitalsAssessment.Core.APIResponse;
+using CapitalsAssessment.Core.Dtos;
+using CapitalsAssessment.Core.Entities;
+using CapitalsAssessment.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapitalsAssessment.Api.Controllers
+{
+    public class ProgramController(IRecruitementProgramRepository employeeProgramRepository) : BaseApiController
+    {
+        private readonly IRecruitementProgramRepository _employeeProgramRepository = employeeProgramRepository;
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResponse<RecruitementProgram>>> CreateProgramOrUpdate(CreateProgramDto input)
+        {
+            var result = await _employeeProgramRepository.CreateNewOrUpdateProgram(input);
+            if (result is not null)
+                return BaseResponse<RecruitementProgram>.Success("Succeeded", result, 200);
+            return BadRequest();
+        }
+
+        /*     [HttpPut]
+             public async Task<ActionResult> UpdateProgram(CreateProgramDto input)
+             {
+                 return Ok();
+             }*/
+
+        [HttpGet]
+        public async Task<ActionResult<BaseResponse<CreateProgramDto>>> GetProgramAndQuestions(string programId)
+        {
+            var result = await _employeeProgramRepository.GetProgram(programId);
+            if (result is not null)
+                return BaseResponse<CreateProgramDto>.Success("Succeeded", result, 200);
+            return BadRequest(result);
+        }
+    }
+}
